687ae32cc89dfb49dbb06ec3a41f053a6479b0b0
Przemek Sipta <przemek.sipta@gmail.com>
2023-12-02 21:02:27 +0100

    feat: task 1 refacotr

--- tasks/2023-12-01/index.ts
+++ tasks/2023-12-01/index.ts
@@ -1,4 +1,3 @@
-
 interface Gift {
     id: number;
     childId: number;
@@ -6,33 +5,27 @@ interface Gift {
 }
 
 export class GiftRegistry {
-    data: Gift[] = []
+    private data: Gift[] = [];
 
-    addGift(childId: Gift['childId'], name: Gift['name']) {
-        this.data.push({id: this.generateId(), childId, name})
+    addGift(childId: Gift['childId'], name: Gift['name']): void {
+        const id = this.generateId();
+        this.data.push({ id, childId, name });
     }
 
-    getGiftsForChild(id: Gift['childId']) {
-        const gifts = this.data.filter(giftItem => giftItem.childId === id);
+    getGiftsForChild(id: Gift['childId']): string[] {
+        return this.data.filter(gift => gift.childId === id).map(gift => gift.name);
+    }
 
-        if(!gifts) {
-            throw new Error('Gift not found')
+    removeGift(childId: Gift['childId'], name: Gift['name']): void {
+        const initialDataLength = this.data.length;
+        this.data = this.data.filter(gift => gift.childId !== childId || gift.name !== name);
+
+        if (this.data.length === initialDataLength) {
+            throw new Error('Gift not found');
         }
-
-        return gifts.map(gift => gift.name)
     }
 
-    removeGift(childId: Gift['childId'], name: Gift['name']) {
-        const giftToDelete = this.data.find(item => item.childId === childId && item.name === name);
-
-        if(!giftToDelete) {
-            throw new Error('Gift not found')
-        }
-
-        this.data = this.data.filter(item => item.id !== giftToDelete.id)
-    }
-
-    private generateId() {
-        return this.data.length === 0 ? 0 : this.data.length + 1
+    private generateId(): number {
+        return this.data.length;
     }
 }
